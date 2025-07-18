8ca6df1d9a42d8c42bd21fa46d91c0844d348a1a
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-07-18 12:33:29 +0200

    file utilities

---
+++ fileutils.py
@@ -0,0 +1,56 @@
+import zipfile
+import requests
+from tqdm import tqdm
+import os
+import shutil
+
+def create_or_clear_directory(path):
+    if os.path.exists(path):
+        # Clear directory
+        print("Clearing directory")
+        shutil.rmtree(path)
+    # Create empty directory
+    os.makedirs(path)
+
+def create_directory(path):
+    # Create empty directory
+    if os.path.exists(path):
+        return
+    else:
+        os.makedirs(path)
+
+def datasetdownload():
+    # URL of the dataset
+    url = "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip"
+    local_filename = "maestro-v3.0.0.zip"
+
+    # Stream download with progress bar
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+
+    with open(local_filename, 'wb') as file, tqdm(
+        desc="Downloading MAESTRO Dataset",
+        total=total_size,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in response.iter_content(block_size):
+            file.write(data)
+            bar.update(len(data))
+
+    # Unzip after download
+    with zipfile.ZipFile(local_filename, 'r') as zip_ref:
+        print("\nExtracting...")
+        zip_ref.extractall("maestro_dataset")
+
+    print("Done! Dataset is in the 'maestro_dataset' folder.")
+
+def get_all_files_recursive(folder):
+    all_files = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            full_path = os.path.join(root, file)
+            all_files.append(full_path)
+    return all_files
