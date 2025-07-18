cff536abb4d07dde2783bbe0a4ffeb20b3e11b66
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-07-18 12:34:10 +0200

    main logic

--- scoreset.py
+++ scoreset.py
@@ -1,44 +1,25 @@
-import requests
-from tqdm import tqdm
-import zipfile
 from midihelper import midi_extract
 from guitarprohelper import makegpro, writegpro
 from expressions import insertexpressions
 from techniques import create_accents
-
-def datasetdownload():
-    # URL of the dataset
-    url = "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip"
-    local_filename = "maestro-v3.0.0.zip"
-
-    # Stream download with progress bar
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024  # 1 Kibibyte
-
-    with open(local_filename, 'wb') as file, tqdm(
-        desc="Downloading MAESTRO Dataset",
-        total=total_size,
-        unit='iB',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in response.iter_content(block_size):
-            file.write(data)
-            bar.update(len(data))
-
-    # Unzip after download
-    with zipfile.ZipFile(local_filename, 'r') as zip_ref:
-        print("\nExtracting...")
-        zip_ref.extractall("maestro_dataset")
-
-    print("Done! Dataset is in the 'maestro_dataset' folder.")
-
+from fileutils import get_all_files_recursive, datasetdownload, create_or_clear_directory, create_directory
+import re
 # Usage
 # datasetdownload()
-create_accents()
-filename = "./MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi"
-duration_note, start_tick, notevalue = midi_extract(filename, 0)
-song = makegpro(duration_note, start_tick, notevalue, [64, 59, 55, 50, 45, 40], 55)
-song = insertexpressions(song)
-writegpro('output', song)
+# create_accents()
+# Example usage:
+gpro_dir = "./maestro_dataset_gpro"
+create_or_clear_directory(gpro_dir)
+files = get_all_files_recursive('./maestro_dataset')
+for f in files: 
+    parts = re.split(r'[\\/]+', f)
+    if len(parts) == 5:
+        if ".midi" in parts[4]:
+            create_directory(gpro_dir + "/" + parts[2] + "/" + parts[3])
+            print("Processing : ", parts[4])
+            duration_note, start_tick, notevalue = midi_extract(f, 0)
+            song = makegpro(duration_note, start_tick, notevalue, [64, 59, 55, 50, 45, 40], 55)
+            song = insertexpressions(song)
+
+            gprofilename = re.split(r'\.', parts[4])
+            writegpro(gpro_dir + "/" + parts[2] + "/" + parts[3] + "/" + gprofilename[0], song)
