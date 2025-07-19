6e6c44de3986ecec898f5fc20387fe62d31205f3
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-07-19 17:39:50 +0200

    giant midi

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from scoreset import scoreset
-from fileutils import zip_directory, datasetdownload
+from scoreset import scoreset, midi_extract
+from fileutils import zip_directory, datasetdownload, datasetdownload_gdown
 from techniques import create_accents
 
 if __name__ == "__main__":
-    # datasetdownload()
-    # create_accents()
-    print("Processing started!")
-    scoreset("./maestro_dataset_gpro", "./maestro_dataset")
-    print("Processing completed!")
+    create_accents()
+    ############################## MAESTRO ###################################
+    print("Maestro dataset : Downloading")
+    datasetdownload("https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip", "maestro-v3.0.0")
+    print("Maestro dataset : Download complete!")
+
+    print("Maestro dataset : Processing started")
+    scoreset("./maestro_dataset_gpro", "./maestro-v3.0.0", [])
+    print("Maestro dataset : Processing completed!")
 
     print("Creating .zip file")
     zip_directory("./maestro_dataset_gpro", "maestro_gpro-v1.0.0.zip")
+    print(".zip file created!")
+    
+    ############################## GIANT-MIDI #################################
+    print("GiantMIDI dataset : Downloading")
+    datasetdownload_gdown("https://drive.google.com/drive/folders/1Stz3CAvMoplo79LR5I3onMWRelCugBYS?usp=sharing", "./midis_dataset")
+    print("GiantMIDI dataset : Download complete")
+
+    print("Maestro dataset : Processing started!")
+    exclude_files = ["Gumpelzhaimer, Adam, Was mein Gott will, das g'scheh allzeit, BTQegBbW13A.mid",
+                     "Stahl, William C., Golden Bell Waltz, gmrKI53VUVQ.mid"]
+    scoreset("./midis_dataset_gpro", "./midis_dataset", exclude_files)
+    print("Maestro dataset : Processing completed!")
+
+    print("Creating .zip file")
+    zip_directory("./midis_dataset_gpro", "midis_gpro-v1.0.0.zip")
     print(".zip file created!")
--- scoreset.py
+++ scoreset.py
@@ -3,14 +3,18 @@ from guitarprohelper import makegpro, writegpro
 from expressions import insertexpressions
 from fileutils import get_all_files_recursive, create_or_clear_directory, create_directory
 import re
+import os
 
-def scoreset(gpro_dir, midi_dir):
+def scoreset(gpro_dir, midi_dir, excluded_filenames):
     gpro_dir = gpro_dir
     create_or_clear_directory(gpro_dir)
     files = get_all_files_recursive(midi_dir)
-    for f in files: 
+    filtered_files = [
+    f for f in files
+        if not (os.path.basename(f) in excluded_filenames)]
+    for f in filtered_files: 
         parts = re.split(r'[\\/]+', f)
-        if ".midi" in parts[-1]:
+        if ".mid" in parts[-1]:
             create_directory(gpro_dir + "/" + parts[-3] + "/" + parts[-2])
             print("Processing : ", parts[-1])
             duration_note, start_tick, notevalue = midi_extract(f, 0)
